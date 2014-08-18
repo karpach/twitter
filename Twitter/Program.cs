c8ce4093aac07b43e2038d5663ba25ed993d6812
Viktar Karpach <karpach@gmail.com>
2014-08-18 16:22:17 -0500

    Added missing Program file

---
+++ Twitter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Twitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var twitter = new Twitter
+            {
+                OAuthConsumerKey = "OAuth Consumer Key",
+                OAuthConsumerSecret = "OAuth Consumer Secret"
+            };
+            IEnumerable<string> twitts = twitter.GetTwitts("karpach96", 10).Result;
+            foreach (var t in twitts)
+            {
+                Console.WriteLine(t+"\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
